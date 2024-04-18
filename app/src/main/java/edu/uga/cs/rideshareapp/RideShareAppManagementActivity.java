c6179e4f6f502abd6dd85e3359a7b469d0e3d62d
“aalexisadkins <ana04657@uga.edu>
2024-04-17 23:26:39 -0400

    did main activity, registration works in firebase, sign in dialog, ,

---
+++ app/src/main/java/edu/uga/cs/rideshareapp/RideShareAppManagementActivity.java
@@ -0,0 +1,107 @@
+package edu.uga.cs.rideshareapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class RideShareAppManagementActivity extends AppCompatActivity {
+    private static final String DEBUG_TAG = "ManagementActivity";
+    private TextView signedInTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ride_share_app_management);
+        Log.d( DEBUG_TAG, "RideShareAppManagementActivity.onCreate()" );
+
+        Button viewRideRequests = findViewById(R.id.button1);
+        Button viewRideOffers = findViewById(R.id.button2);
+        signedInTextView = findViewById( R.id.textView3 );
+
+
+        viewRideRequests.setOnClickListener( new RideRequestButtonClickListener() );
+        viewRideOffers.setOnClickListener( new RideOffersButtonClickListener() );
+
+    // Setup a listener for a change in the sign in status (authentication status change)
+    // when it is invoked, check if a user is signed in and update the UI text view string,
+    // as needed.
+        FirebaseAuth.getInstance().addAuthStateListener(new FirebaseAuth.AuthStateListener() {
+        @Override
+        public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+            FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+            if( currentUser != null ) {
+                // User is signed in
+                Log.d(DEBUG_TAG, "onAuthStateChanged:signed_in:" + currentUser.getUid());
+                String userEmail = currentUser.getEmail();
+                signedInTextView.setText( "Signed in as: " + userEmail );
+            } else {
+                // User is signed out
+                Log.d( DEBUG_TAG, "onAuthStateChanged:signed_out" );
+                signedInTextView.setText( "Signed in as: not signed in" );
+            }
+        }
+    });
+ }
+
+
+    private class RideRequestButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(view.getContext(), RideRequestActivity.class);
+            view.getContext().startActivity( intent );
+        }
+    }
+
+    private class RideOffersButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(view.getContext(), RideOffersActivity.class);
+            view.getContext().startActivity(intent);
+        }
+    }
+
+    // These activity callback methods are not needed and are for edational purposes only
+    @Override
+    protected void onStart() {
+        Log.d( DEBUG_TAG, "RideShareApp: ManagementActivity.onStart()" );
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d( DEBUG_TAG, "RideShareApp: ManagementActivity.onResume()" );
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d( DEBUG_TAG, "RideShareApp: ManagementActivity.onPause()" );
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d( DEBUG_TAG, "RideShareApp: ManagementActivity.onStop()" );
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d( DEBUG_TAG, "RideShareApp: ManagementActivity.onDestroy()" );
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.d( DEBUG_TAG, "RideShareApp: ManagementActivity.onRestart()" );
+        super.onRestart();
+    }
+}
